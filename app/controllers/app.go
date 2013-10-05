21ae27cd9049022cbcd4a703e04fcdb829f5fe2b
Manoj Kintali <vmkintali@gmail.com>
2013-10-05 16:50:29 -0400

    adding db and leaderboard

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"github.com/coopernurse/gorp"
+	"github.com/robfig/revel"
+	db "github.com/robfig/revel/modules/db/app"
+)
+
+var (
+	Dbm *gorp.DbMap
+)
 
 type App struct {
 	*revel.Controller
@@ -8,4 +16,15 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+type Leaderboard struct {
+	id   int64
+	name string
+}
+
+func Init() {
+	db.Init()
+	Dbm = &gorp.DbMap{Db: db.Db, Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	Dbm.AddTableWithName(Leaderboard{}, "leaderboard.leaderboards")
 }
