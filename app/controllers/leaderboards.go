9683a933ca38a37810ff5ec833bf4e83be4ea025
Manoj Kintali <vmkintali@gmail.com>
2013-10-05 19:58:12 -0400

    Code cleanup

--- app/controllers/leaderboards.go
+++ app/controllers/leaderboards.go
@@ -1,29 +1,15 @@
 package controllers
 
-import (
-	"github.com/robfig/revel"
-	"fmt"
-)
+import "github.com/robfig/revel"
 
 type LeaderBoards struct {
 	*revel.Controller
 }
 
-//func (c Application) CreateLeaderBoard(leaderBoard models.leaderBoard) revel.Result
-//{
-
-//}
-
-func (c LeaderBoards) ViewLeaderBoards() revel.Result {
+func (c LeaderBoards) ViewBoards() revel.Result {
 	return c.RenderTemplate("App/leaderboards.html")
 }
 
-func (c LeaderBoards) AddLeaderBoard() revel.Result {
+func (c LeaderBoards) AddBoard() revel.Result {
 	return c.RenderTemplate("App/addleaderboard.html")
-}
-
-func (c LeaderBoards) InsertLeaderBoard(newLeaderBoard string) revel.Result {
-	fmt.Println("inserting", newLeaderBoard);
-	return c.RenderTemplate("App/leaderboards.html")
-	
 }
