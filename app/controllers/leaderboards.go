73f9aae865af1a25019e6eda82e0bc0f574926db
Manoj Kintali <vmkintali@gmail.com>
2013-10-05 20:37:00 -0400

    Add a leaderboard.html to view a single leaderboard

--- app/controllers/leaderboards.go
+++ app/controllers/leaderboards.go
@@ -10,6 +10,18 @@ type LeaderBoards struct {
 	*revel.Controller
 }
 
+// TODO (mkintali): This is garbage, clean this shit up
+func (c LeaderBoards) ViewBoard(boardId int64) revel.Result {
+	var boards []*models.Leaderboard
+	_, err := Dbm.Select(&boards, `select * from leaderboard.leaderboards where id = ?`, boardId)
+	if err != nil {
+		glog.Error(err)
+	}
+
+	c.RenderArgs["board"] = boards[0]
+	return c.RenderTemplate("Leaderboards/leaderboard.html")
+}
+
 func (c LeaderBoards) ViewBoards() revel.Result {
 	var boards []*models.Leaderboard
 	_, err := Dbm.Select(&boards, `select * from leaderboard.leaderboards`)
