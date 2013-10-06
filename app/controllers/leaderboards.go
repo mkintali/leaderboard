fafbb2d0a5a055ec41bb565ebf130e5cc7823abb
Manoj Kintali <vmkintali@gmail.com>
2013-10-05 20:14:07 -0400

    Retrieve and view leaderboards from database.

--- app/controllers/leaderboards.go
+++ app/controllers/leaderboards.go
@@ -1,15 +1,25 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"github.com/golang/glog"
+	"github.com/robfig/revel"
+	"leaderboard/app/models"
+)
 
 type LeaderBoards struct {
 	*revel.Controller
 }
 
 func (c LeaderBoards) ViewBoards() revel.Result {
-	return c.RenderTemplate("App/leaderboards.html")
+	var boards []*models.Leaderboard
+	_, err := Dbm.Select(&boards, `select * from leaderboard.leaderboards`)
+	if err != nil {
+		glog.Error(err)
+	}
+	c.RenderArgs["boards"] = boards
+	return c.RenderTemplate("Leaderboards/leaderboards.html")
 }
 
 func (c LeaderBoards) AddBoard() revel.Result {
-	return c.RenderTemplate("App/addleaderboard.html")
+	return c.RenderTemplate("Leaderboards/addleaderboard.html")
 }
